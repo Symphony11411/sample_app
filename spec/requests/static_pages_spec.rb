f98f76be2317ac0c66e8f72925b2b85c0303a0d3
Angela Lenox <alenox@madisoncollege.edu>
2013-04-08 15:16:23 -0500

    Finish user edit, update, index, and destroy actions

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -18,6 +18,7 @@ describe "Static pages" do
     click_link "Contact"
     page.should have_selector 'title', text: full_title('Contact')
     click_link "Home"
+    page.should have_selector 'title', text: full_title('')
     click_link "Sign up now!"
     page.should have_selector 'title', text: full_title('Sign up')
     click_link "sample app"
